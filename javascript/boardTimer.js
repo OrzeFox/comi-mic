49ee55e7b81b245abaa132b16a038577f64a3f42
denizozerdogan <131254999+denizozerdogan@users.noreply.github.com>
2023-05-17 19:12:06 +0200

    ana's timer sounds

--- javascript/boardTimer.js
+++ javascript/boardTimer.js
@@ -2,12 +2,15 @@ var timerElement = document.getElementById('timer');
 var timeLeft = 120; // 2 minutes in seconds
 var countdown;
 var startTimerButton = document.getElementById('startTimer');
+var audio = document.getElementById('audio'); 
+
 
 function startTimer() {
     clearInterval(countdown);
+    audio.play();
 
     var minutes, seconds;
-    timeLeft = 120;//if press start recount again from zero
+    timeLeft = 120//if press start recount again from zero
 
     countdown = setInterval(function() {
     minutes = Math.floor(timeLeft / 60);
@@ -20,13 +23,24 @@ function startTimer() {
 
     if (timeLeft > 0) {
         timeLeft--;
+        
     } else {
         clearInterval(countdown);
         timerElement.textContent = "Time's up!";
         timerElement.style.backgroundColor = 'rgba(243, 101, 93, 0.8)';
-    }
-    }, 1000);
+        
+    }if (timeLeft <=0) {
+       setTimeout (function(){
+        audio.pause();
+       },1000)
+       setTimeout(function(){
+        alarm.play();
+       },1000)
+
+       }
+    }, 1000); 
     timerElement.style.backgroundColor = ('rgba(157, 199, 5, 0.5)');
+    
 }
 
 startTimerButton.addEventListener('click', startTimer);
