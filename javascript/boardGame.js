435efb9cc727476b4c6290927f2ab7f794303d0e
Yumi-Namie <rn.yumi@gmail.com>
2023-05-18 23:51:49 +0200

    rotation boardgame

--- javascript/boardGame.js
+++ javascript/boardGame.js
@@ -9,14 +9,15 @@ const btnPlay = document.getElementById('btnPlay');
 const btnFinish = document.querySelector('.game-btn button:nth-of-type(1)');
 const btnNext = document.querySelector('.game-btn button:nth-of-type(2)');
 const resultGame = document.getElementById('resultGame');
+const boardGame = document.querySelector('.boardGame');
 
 
 let objectListGame = objects;
 let actionListGame = actions;
-let randomSelectionTimer;
-let selectionInterval = 100;
-const selectionTime = 800;
-let stopSelectionTimeout;
+let randomSelectionTimer; //efecto randomwords
+let selectionInterval = 90; //efecto randomwords
+const selectionTime = 1000; //efecto randomwords
+let stopSelectionTimeout; //efecto randomwords
 
 // Ocultar challengeDisplay
 challengeDisplay.style.display = 'none';
@@ -60,6 +61,10 @@ function stopRandomSelection() {
 btnPlay.addEventListener('click', function() {
   readyDisplay.style.display = 'none';
   challengeDisplay.style.display = 'block';
+  challengeDisplay.style.transform = 'rotateY(180deg)';
+  boardGame.style.transition = 'all 0.8s ease';
+  boardGame.style.transform = 'rotateY(180deg)';
+
   startRandomSelection();
 });
 
@@ -70,8 +75,10 @@ btnFinish.addEventListener('click', () => {
   readyDisplay.style.alignItems = 'center';
   challengeDisplay.style.display = 'none';
 
-  clearInterval(randomSelectionTimer);
-  clearTimeout(stopSelectionTimeout);
+  //transition
+  boardGame.style.transition = 'all 0.8s ease';
+  boardGame.style.transform = 'rotateY(360deg)';
+  readyDisplay.style.transform = 'rotateY(360deg)';
 
   setTimeout(() => {
     location.reload();
@@ -91,94 +98,6 @@ btnNext.addEventListener('click', () => {
 
   startRandomSelection();
 });
-// import { objects } from './script.js';
-// import { actions } from './script.js';
-
-// // Elementos importantes
-// const readyDisplay = document.querySelector('.readyDisplay');
-// const challengeDisplay = document.querySelector('.challengeDisplay');
-// const btnPlay = document.getElementById('btnPlay');
-// const btnFinish = document.querySelector('.game-btn button:nth-of-type(1)');
-// const btnNext = document.querySelector('.game-btn button:nth-of-type(2)');
-// const resultGame = document.getElementById('resultGame');
-
-// let objectListGame = objects;
-// let actionListGame = actions;
-// let selectionInterval = 100;
-// let stopSelectionTimeout;
-
-// // Ocultar challengeDisplay inicialmente
-// challengeDisplay.style.display = 'none';
-
-// function startRandomSelection() {
-//   randomSelectionTimer = setInterval(selectRandomWords, selectionInterval);
-// }
-// function startRandomSelection() {
-//   randomSelectionTimer = setInterval(selectRandomWords, selectionInterval);
-// }
-
-// function selectRandomWords() {
-//   const randomIndex1 = Math.floor(Math.random() * objectListGame.length);
-//   const randomObject = objectListGame[randomIndex1];
-//   const randomIndex2 = Math.floor(Math.random() * actionListGame.length);
-//   const randomAction = actionListGame[randomIndex2];
-
-//   resultGame.innerText = `You've got ${randomObject} and your action is ${randomAction}!`;
-// }
-
-// function stopRandomSelection() {
-//   clearInterval(randomSelectionTimer);
-//   clearTimeout(stopSelectionTimeout);
-
-//   const randomIndex1 = Math.floor(Math.random() * objectListGame.length);
-//   const randomObject = objectListGame[randomIndex1];
-//   const randomIndex2 = Math.floor(Math.random() * actionListGame.length);
-//   const randomAction = actionListGame[randomIndex2];
-
-//   resultGame.innerText = `You've got ${randomObject} and your action is ${randomAction}!`;
-
-//   objectListGame.splice(randomIndex1, 1);
-//   actionListGame.splice(randomIndex2, 1);
-
-//   if (objectListGame.length === 0) {
-//     btnNext.disabled = true;
-//     resultGame.innerText = `"THE END!"`;
-//   }
-// }
-
-// // Evento "Ready"
-// btnPlay.addEventListener('click', function() {
-//   readyDisplay.style.display = 'none';
-//   challengeDisplay.style.display = 'block';
-//   startRandomSelection();
-
-//   stopSelectionTimeout = setTimeout(stopRandomSelection, 100); 
-// });
-
-
-
-// // Btn "Finish"
-// btnFinish.addEventListener('click', () => {
-
-//   readyDisplay.style.display = 'flex'; // Exibir display ready 
-//   readyDisplay.style.justifyContent = 'center';
-//   readyDisplay.style.alignItems = 'center';
-//   challengeDisplay.style.display = 'none';
-
-//   location.reload();
-  
-// });
-
-// // Btn "Next"
-// btnNext.addEventListener('click', () => {
-//   if (objectListGame.length === 0) {
-//     btnNext.disabled = true;
-//     resultGame.innerText = `"THE END!"`;
-//     return;
-//   }
-
-//   startRandomSelection();
-// });
 
 
 
